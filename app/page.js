5ab2f8143a81957800afb8b2ad4194614141ce6d
Arturo Cuicas <arturo.cuicas@gmail.com>
2025-01-29 21:13:34 -0600

    Finish Hero Section

--- app/page.js
+++ app/page.js
@@ -1,101 +1,90 @@
 import Image from "next/image";
+import Hero from "@/app/hero-1.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <nav className="relative mx-auto p-3 bg-red-500">
+        <div className="flex items-center justify-between space-x-20 my-3">
+          <div className="z-30 w-10">
+            <img src="favicon.ico" alt="logo" id="logo" className="img-fluid"/>
+          </div>
+          <div className="hidden items-center space-x-10 uppercase text-white md:flex">
+            <a href="#feature" className="tracking-widset" id="feature">
+              feature
+            </a>
+            <a href="#resume" className="tracking-widset" id="resume">
+              resume
+            </a>
+            <a href="#portfolio" className="tracking-widset" id="portfolio">
+              portfolio
+            </a>
+            <a href="#blog" className="tracking-widset" id="blog">
+              blog
+            </a>
+            <a href="#contact" className="tracking-widset" id="contact">
+              contact
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </nav>
+
+      <section id="hero" className="hero">
+        <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row lg:mb-0">
+          <div className="flex flex-col space-y-10 lg:mt-16 lg:w-1/2">
+            <p className="uppercase">
+              Live in motion!
+            </p>
+            <h1 className="text-3xl font-semibold lg:text-6xl">
+              Hi, I’m <span className="text-red-500">Arturo Cuicas</span>
+            </h1>
+            <h2 className="text-2xl font-semibold lg:text-4xl">
+              a Tech Lead.
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, odio, recusandae. Assumenda
+              laudantium possimus provident quidem sequi tempore voluptatibus! Accusantium consequuntur dolore facilis
+              mollitia neque nisi quasi sit. Ab, distinctio!
+            </p>
+            <div className="container relative mx-auto p-6 bg-red-500">
+              <div className="flex items-center justify-between space-x-20 my-6">
+                find with me
+              </div>
+            </div>
+          </div>
+          <div className="relative mx-auto lg:mx-0 lg:mb-0 lg:w-1/2">
+            <img src={Hero.src} alt="me"
+                 className="relative mx-auto lg:w-3/4 z-10 lg:top-24 xl:top-0 overflow-x-visible"/>
+          </div>
+
+        </div>
+      </section>
+
+      <section id="features">
+        <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row lg:mb-0">
+          features
+        </div>
+      </section>
+      <section id="resume">
+        <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row lg:mb-0">
+          resume
+        </div>
+      </section>
+      <section id="portfolio">
+        <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row lg:mb-0">
+          portfolio
+        </div>
+      </section>
+      <section id="blog">
+        <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row lg:mb-0">
+          blog
+        </div>
+      </section>
+      <section id="contact">
+        <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row lg:mb-0">
+          contact
+        </div>
+      </section>
+    </>
   );
 }
